9746e119ad0f13b06537d963e6ece64c6db1b778
Christian Wall <Christian.Wall@buetema-ag.de>
2023-02-02 15:56:17 +0100

    simplified sampel

--- MauiKeyboardBug/MainPage.xaml.cs
+++ MauiKeyboardBug/MainPage.xaml.cs
@@ -5,29 +5,40 @@ public partial class MainPage : ContentPage
   public MainPage()
   {
     InitializeComponent();
-    myWebView.Navigating += MyWebView_Navigating;
-    myWebView.Source = new HtmlWebViewSource() { Html = @"<html>
-<head>
-<meta name=""viewport"" content=""width=320, user-scalable=no"" />
-<script>
-  function doSomeWork() {
-    document.getElementById('myinput').focus();
-    location.href=""secondpage"";
-  }
-</script>
-</head>
-<body style='background-color:yellow'><input id='myinput'><br/><br/><input type='button' value='click me' onclick='doSomeWork();'/></body></html>" };
+    myWebView.Source = new UrlWebViewSource() { Url = "https://www.bing.com" };
+//    myWebView.Source = new HtmlWebViewSource() { Html = @"<html>
+//<head>
+//<meta name=""viewport"" content=""width=320, user-scalable=no"" />
+//</head>
+//<body style='background-color:yellow'><h1>Input inside webview</h1><input id='myinput'><br><br>After tapping the timer button tap into the input field to open the keyboard.</body></html>" };
 
   }
 
-  private async void MyWebView_Navigating(object sender, WebNavigatingEventArgs e)
+  private async void Button_Clicked(object sender, EventArgs e)
   {
-    if (e.Url.EndsWith("secondpage", StringComparison.OrdinalIgnoreCase))
+    TimerButton.Text = "3";
+    await Task.Delay(1000).ContinueWith(async _ =>
     {
-      e.Cancel = true;
-      await myWebView.EvaluateJavaScriptAsync(@"");
-      await Navigation.PushModalAsync(new SecondPage());
-    }
+      await MainThread.InvokeOnMainThreadAsync(async () =>
+      {
+        TimerButton.Text= "2";
+        await Task.Delay(1000).ContinueWith(async _ =>
+        {
+          await MainThread.InvokeOnMainThreadAsync(async () =>
+          {
+            TimerButton.Text = "1";
+            await Task.Delay(1000).ContinueWith(async _ =>
+            {
+              await MainThread.InvokeOnMainThreadAsync(async () =>
+              {
+                await Navigation.PushModalAsync(new SecondPage());
+                TimerButton.Text = "Open new Page in 3 seconds";
+              });
+            });
+          });
+        });
+      });
+    });
   }
 }
 
